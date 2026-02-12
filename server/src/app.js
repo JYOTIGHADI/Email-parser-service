24350b0cf3b3fc943c5f9aea185ba27a0a81f773
basic-html.css <basic-html.css.in>
2026-02-13 04:47:07 +0530

    update in Email parser services

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { errorHandler } from "./middleware/error.middleware.js";
+import errorHandler  from "./middleware/error.middleware.js";
 import emailRouter from "./routes/error.routes.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/api/emails", emailRouter);
 
--- server/src/middleware/error.middleware.js
+++ server/src/middleware/error.middleware.js
@@ -1,9 +1,14 @@
-export const errorHandler = (err, req, res) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
 
-  return res.status(statusCode).json({
-    success: false,
-    message,
-  });
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  if (res && typeof res.status === "function") {
+    res.status(500).json({
+      message: err.message || "Internal Server Error",
+    });
+  } else {
+    console.error("res.status is not a function! Check Express app setup.");
+  }
 };
+
+export default errorHandler;
